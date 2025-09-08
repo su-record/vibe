58f3b25b281fc81b65749e28c67383ac6b2f76a9
su-record <su.lifestory@gmail.com>
2025-09-08 18:46:24 +0900

    vercel error

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: false,
-  generateEtags: false,
-  poweredByHeader: false,
-  experimental: {
-    appDir: true
-  }
 }
 
 module.exports = nextConfig
